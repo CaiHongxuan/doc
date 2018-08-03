578263888d6342d734be8b1a4a6eb510e17542c6
1203673288@qq.com <1203673288@qq.com>
2018-08-03 18:11:05 +0800

    完善文档详情页面

--- src/components/Detail.vue
+++ src/components/Detail.vue
@@ -2,7 +2,82 @@
 
     <el-container>
         <el-main>
-            <h1 class="title">文档详情</h1>
+            <h1 class="title">{{title}}</h1>
+
+            <el-form :inline="true" label-position="left">
+                <el-row>
+                    <el-form-item label="接口地址：" v-if="type == 1">
+                        <pre>{{ url }}</pre>
+                    </el-form-item>
+                    <el-form-item label="请求方式：" v-if="type == 1">
+                        {{ method }}
+                    </el-form-item>
+                    <el-form-item label="开发状态：" v-if="type == 1">
+                        {{ status }}
+                    </el-form-item>
+                    <el-form-item label="序号：">
+                        {{ sort }}
+                    </el-form-item>
+                    <el-form-item label="版本：">
+                        {{ version }}
+                    </el-form-item>
+                </el-row>
+
+                <el-tabs v-model="activeName" class="parameters_field" v-if="type == 1 && (parameters.length || headers.length)">
+                    <el-tab-pane label="请求参数（Query）" v-if="parameters.length" name="parameters">
+
+                        <el-table
+                            :data="parameters"
+                            style="width: 100%">
+                            <el-table-column
+                                prop="name"
+                                label="参数名称"
+                                width="180">
+                            </el-table-column>
+                            <el-table-column
+                                prop="is_must"
+                                label="是否必填"
+                                width="180">
+                            </el-table-column>
+                            <el-table-column
+                                prop="type"
+                                label="参数类型">
+                            </el-table-column>
+                            <el-table-column
+                                prop="remark"
+                                label="备注">
+                            </el-table-column>
+                        </el-table>
+
+                    </el-tab-pane>
+                    <el-tab-pane label="头部（Headers）" v-if="headers.length" name="headers">
+
+                        <el-table
+                            :data="headers"
+                            style="width: 100%">
+                            <el-table-column
+                                prop="name"
+                                label="参数名称"
+                                width="180">
+                            </el-table-column>
+                            <el-table-column
+                                prop="is_must"
+                                label="是否必填"
+                                width="180">
+                            </el-table-column>
+                            <el-table-column
+                                prop="type"
+                                label="参数类型">
+                            </el-table-column>
+                            <el-table-column
+                                prop="remark"
+                                label="备注">
+                            </el-table-column>
+                        </el-table>
+
+                    </el-tab-pane>
+                </el-tabs>
+            </el-form>
 
             <mavon-editor
                 v-model="content"
@@ -21,11 +96,24 @@
 </template>
 
 <script>
+
     export default {
         name: 'Detail',
         data () {
             return {
+                type: "",
+                title: "",
                 content: "",
+                url: "",
+                status: "",
+                method: "",
+                version: "",
+                sort: "",
+                created_by: "",
+                updated_at: "",
+                activeName: 'parameters',
+                parameters: [],
+                headers: [],
             }
         },
         methods: {
@@ -33,7 +121,18 @@
                 let that = this;
                 that.$axios.get('/docs/' + id, {}).then(function(response){
                     if (response.status == 200 && response.data.code == 0) {
+                        that.type = response.data.data.type;
+                        that.title = response.data.data.title;
                         that.content = response.data.data.content;
+                        that.url = response.data.data.url;
+                        that.status = response.data.data.status_plan;
+                        that.method = response.data.data.method_plan;
+                        that.version = response.data.data.version;
+                        that.sort = response.data.data.sort;
+                        that.created_by = response.data.data.created_by.name;
+                        that.updated_at = response.data.data.updated_at;
+                        that.parameters = JSON.parse(response.data.data.arguments)['parameters'];
+                        that.headers = JSON.parse(response.data.data.arguments)['headers'];
                     } else if (response.status === -404) {
                         that.$message.error(response.msg);
                     } else {
