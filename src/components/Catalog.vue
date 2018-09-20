ae4af6f48995a66dfee51859aac7b6d0aa5032fa
蔡俊贤 <1203673288@qq.com>
2018-09-20 22:51:19 +0800

    完成目录管理

--- src/components/Catalog.vue
+++ src/components/Catalog.vue
@@ -2,7 +2,7 @@
 
     <el-container>
         <el-card class="box-card center-card">
-            <el-button type="text" @click="dialogform.dialogFormVisible = true">添加目录</el-button>
+            <el-button type="text" @click="dialogform.dialogFormVisible = dialogform.method = true">添加目录</el-button>
             <el-button type="text" onclick="history.go(-1)">返回</el-button>
             <div class="text item">
                 <el-table
@@ -12,7 +12,7 @@
                     border
                     style="width: 100%">
                     <el-table-column
-                        prop="name"
+                        prop="newname"
                         label="目录名"
                         width="160">
                     </el-table-column>
@@ -29,7 +29,7 @@
                     <el-table-column
                         label="操作">
                         <template slot-scope="scope">
-                            <el-button @click="editCat(scope.row)" type="text" size="small">编辑</el-button>
+                            <el-button @click="showEditCat(scope.row)" type="text" size="small">编辑</el-button>
                             <el-button @click="delCat(scope.row.id)" type="text" size="small">删除</el-button>
                         </template>
                     </el-table-column>
@@ -37,7 +37,7 @@
             </div>
         </el-card>
 
-        <el-dialog title="新增目录" :visible.sync="dialogform.dialogFormVisible" :width="dialogform.dialogWidth">
+        <el-dialog :title="dialogform.method ? '新增目录' : '编辑目录'" :visible.sync="dialogform.dialogFormVisible" :width="dialogform.dialogWidth">
             <el-form :model="dialogform">
                 <el-form-item label="上级目录" :label-width="dialogform.formLabelWidth">
                     <el-cascader
@@ -57,7 +57,7 @@
             </el-form>
             <div slot="footer" class="dialog-footer">
                 <el-button @click="dialogform.dialogFormVisible = false">取 消</el-button>
-                <el-button type="primary" @click="addCat">确 定</el-button>
+                <el-button type="primary" @click="dialogform.method ? addCat : editCat(dialogform.id)">确 定</el-button>
             </div>
         </el-dialog>
     </el-container>
@@ -73,6 +73,8 @@
                 catalogs: [],
                 showDelTip: false,
                 dialogform: {
+                    method: true, // true 新增目录；false 编辑目录
+                    id: '',
                     name: '',
                     parents: [],
                     sort: 99,
@@ -88,21 +90,20 @@
             }
         },
         methods: {
+            // 给顶级目录增加背景色
             tableRowClassName({row, rowIndex}) {
-                if (rowIndex === 1) {
-                    return 'warning-row';
-                } else if (rowIndex === 3) {
+                if (row.parent_id == 0) {
                     return 'success-row';
                 }
                 return '';
             },
             // 递归处理目录（树形目录转化成一级目录）
-            recurCat(Cats, is_children = false) {
+            recurCat(Cats, level = 0) {
                 for (var i = 0; i < Cats.length; i++) {
-                    is_children && (Cats[i].name = '-- ' + Cats[i].name);
+                    Cats[i].newname = level ? (Array(level+1).join('--') + ' ' + Cats[i].name) : Cats[i].name;
                     this.catalogs.push(Cats[i]);
                     if (Cats[i].children && Cats[i].children.length) {
-                        this.recurCat(Cats[i].children, true);
+                        this.recurCat(Cats[i].children, level + 1);
                     }
                 }
             },
@@ -116,7 +117,7 @@
                         that.catalogs = [];
                         // 采用JSON.parse与JSON.stringify防止浅拷贝
                         that.dialogform.options = JSON.parse(JSON.stringify(data));
-                        that.dialogform.options.unshift({id: 0, name:"顶级目录"});
+                        that.dialogform.options.unshift({id: "0", name:"顶级目录"});
                         that.recurCat(data);
                     } else if (response.status === -404) {
                         that.$message.error(response.msg);
@@ -150,8 +151,37 @@
                 });
                 this.dialogform.dialogFormVisible = false;
             },
+            // 显示编辑目录
+            showEditCat(Cat) {
+                this.dialogform.method = false;
+                this.dialogform.id = Cat.id;
+                this.dialogform.name = Cat.name;
+                this.dialogform.parents = Cat.parent_ids.split(',');
+                this.dialogform.sort = Cat.sort;
+                this.dialogform.dialogFormVisible = true;
+            },
+            // 更新目录
             editCat(CatId) {
-                // todo
+                let that = this;
+                let data = {
+                    name: that.dialogform.name,
+                    parent_ids: that.dialogform.parents,
+                    sort: that.dialogform.sort,
+                    pro_id: that.$route.params.id
+                };
+                that.$axios.put('/catalogs/' + CatId, data).then(function(response){
+                    if (response.status == 200 && response.data.code == 0) {
+                        that.$message.success('更新成功');
+                        that.loadCats(that.$route.params.id);
+                    } else if (response.status === -404) {
+                        that.$message.error(response.msg);
+                    } else {
+                        that.$message.error(response.data.msg);
+                    }
+                }).catch(function(response){
+                    console.log(response); // 发生异常错误时执行的代码
+                });
+                this.dialogform.dialogFormVisible = false;
             },
             // 删除目录
             delCat (CatId) {
